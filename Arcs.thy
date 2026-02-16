6f4e6736453b8d1c9ce602c62eb3a43f84fc0635
Deniz İmge <denizimgeavci@gmail.com>
2026-02-16 18:50:31 +0100

    Continued on the arcs theory, fixed the maximal_intersecting_n_arcs theorem

--- Arcs.thy
+++ Arcs.thy
@@ -16,6 +16,12 @@ definition arcs_of_cycle :: "'a list \<Rightarrow> 'a list set" where
 definition n_arcs_of_cycle :: "'a list \<Rightarrow> nat \<Rightarrow> 'a list set" where
   "n_arcs_of_cycle cycle n = {arc \<in> arcs_of_cycle cycle. length arc = n}"
 
+definition generate_n_arc :: "'a list \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> 'a list" where
+  "generate_n_arc cycle index arc_size = take arc_size (rotate index cycle)"
+
+definition n_arc_indices :: "'a list \<Rightarrow> 'a list list \<Rightarrow> nat \<Rightarrow> nat set" where
+  "n_arc_indices cycle arcs arc_size = {n :: nat. n < length cycle \<and> generate_n_arc cycle n arc_size \<in> set arcs}"
+
 definition intersecting_arcs :: "'a list list \<Rightarrow> 'a list  \<Rightarrow> bool" where
   "intersecting_arcs arcs cycle \<longleftrightarrow> (\<forall>arc \<in> set arcs. (arc_of_cycle arc cycle) \<and> (\<forall>other_arc \<in> set arcs. set arc \<inter> set other_arc \<noteq> {}))"
 
@@ -35,8 +41,73 @@ locale arc_context =
   assumes distinct_arcs: "distinct arcs"
 begin
 
+
+lemma unique_index_unique_arc:
+  fixes n1 n2 :: nat 
+  assumes "n1 < length cycle"
+  assumes "n2 < length cycle"
+  shows "n1 \<noteq> n2 \<longleftrightarrow> generate_n_arc cycle n1 arc_size \<noteq> generate_n_arc cycle n2 arc_size"
+  by (metis Ex_list_of_length assms(1,2)
+      cycle_elements_distinct cycle_size distinct_conv_nth
+      generate_n_arc_def hd_rotate_conv_nth hd_take
+      length_greater_0_conv less_not_refl minimal_arc_size
+      mod_less one_eq_numeral_iff order_less_le_trans
+      zero_neq_numeral)
+
+
+lemma generating_index_exists: "arc \<in> set arcs \<longrightarrow> (\<exists>index < length cycle. generate_n_arc cycle index arc_size = arc)"
+  by (metis arc_of_cycle_def fixed_arc_size generate_n_arc_def n_arc_of_cycle_def)
+
+lemma get_generator_index:
+  fixes arc :: "'a list"
+  assumes "arc \<in> set arcs"
+  obtains index where "generate_n_arc cycle index arc_size = arc \<and> index < length cycle" 
+  using generating_index_exists assms by auto
+
+lemma unique_generating_indices:
+  fixes arc1 arc2:: "'a list"
+  fixes index1 index2 :: "nat"
+  assumes "arc1 \<in> set arcs"
+  assumes "arc2 \<in> set arcs"
+  assumes "index1 < length cycle"
+  assumes "index2 < length cycle"
+  assumes "generate_n_arc cycle index1 arc_size = arc1"
+  assumes "generate_n_arc cycle index2 arc_size = arc2"
+  shows "index1 = index2 \<longleftrightarrow> arc1 = arc2" using assms(3,4,5,6) unique_index_unique_arc by blast
+
+
+lemma arc_indices_are_indices: 
+  shows "\<forall>index \<in> n_arc_indices cycle arcs arc_size. \<exists>arc \<in> set arcs. (generate_n_arc cycle index arc_size = arc)" by (simp add: n_arc_indices_def)
+
+lemma arc_indices_exist: "\<forall>arc \<in> set arcs. \<exists>index \<in> n_arc_indices cycle arcs arc_size. (generate_n_arc cycle index arc_size = arc)" 
+  by (metis (no_types, lifting) get_generator_index mem_Collect_eq n_arc_indices_def)
+
+lemma generator_index_bijection: "set arcs =  (\<lambda>index. generate_n_arc cycle index arc_size) ` (n_arc_indices cycle arcs arc_size)" using arc_indices_are_indices arc_indices_exist by fastforce
+
+lemma index_set_size: "length arcs = card (n_arc_indices cycle arcs arc_size)"
+proof -
+  have "length arcs = card (set arcs)" using distinct_arcs by (simp add: distinct_card)
+  then have "... = card (n_arc_indices cycle arcs arc_size)" sorry
+  show "length arcs = card (n_arc_indices cycle arcs arc_size)"  sorry
+qed
+
+
+lemma index_distance:
+  fixes n1 n2 :: nat
+  assumes "n1 < length cycle"
+  assumes "n2 < length cycle"
+  shows "n1 \<le> n2 \<and> n2 < n1 + arc_size \<longrightarrow> (cycle ! n2) \<in> set (generate_n_arc cycle n1 arc_size)" sorry
+
 theorem intersecting_n_arcs_upper_limit: "length arcs \<le> arc_size" sorry
 
-theorem maximal_intersecting_n_arcs: "\<exists>arcs. length arcs = arc_size" sorry
+theorem maximal_intersecting_n_arcs: "\<exists>arcs :: 'a list list. intersecting_n_arcs arcs cycle arc_size \<and> distinct arcs \<and> length arcs = arc_size" 
+proof
+  define indices where "indices = [0..<arc_size]"
+  define arcs where "arcs = map (\<lambda>index. generate_n_arc cycle index arc_size) indices"
+  show "intersecting_n_arcs arcs cycle arc_size \<and> distinct arcs \<and> length arcs = arc_size" sorry
+qed
+
+
+end
 
 end
